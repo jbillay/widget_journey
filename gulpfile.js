637501aa067954c173ef063edbb40af43112b67c
jbillay <jbillay@gmail.com>
2015-12-08 16:58:04 +0100

    fix heroku port issue

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var gulp = require('gulp'),
 gulp.task('connect', function () {
     connect.server({
         root: './',
-        port: 5000
+        port: process.env.PORT || 5000
     })
 });
 
